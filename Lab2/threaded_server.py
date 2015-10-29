1482dbfbab3acaac46e14f4b87712bdd006bd27e
Julius <mereckaj@tcd.ie>
2015-10-29 11:46:47 -0400

    Various changes in order to ensure compataibility w/ the test server

--- Lab2/threaded_server.py
+++ Lab2/threaded_server.py
@@ -40,23 +40,21 @@ class ThreadPoolMixIn(SocketServer.ThreadingMixIn):
 	def finish_request(self, request, client_address):
 		#Recieve data from client
 		data = request.recv(1024)
-		#If it sends a kill request, kill the server (well, try to anyway, it doesn't work)
+		#If it sends a kill request, kill the server (well, try to anyway)
 		if "KILL_SERVICE" in data:
 			request.sendall("KELL_SERVICE recieved, shutting down")
 			print "Killing server..."
 			killer_thread = threading.Thread(target = self.shutdown)
 			killer_thread.start()
+		if "HELO" not in data:
+			data = "Message not formateed correctly"
 
 		curr_thread = threading.current_thread()
-		#Can't print the client's actual IP for some reason
-		response = " {0} IP: {1}\n Port: {2}\n Student ID: {3}\n {4}\n\n".format(data, request.getsockname(), PORT, self.student_id, curr_thread.name)
+		response = "{0}IP:{1}\nPort:{2}\nStudentID:{3}\n".format(data, request.getsockname()[0], PORT, self.student_id)
 		request.sendall(response)
 
-	#For some reason, all attempts to shut down the main loop fail
-	#None of the provided methods work
-	#Who would've thought killing the server would be harder than creating it
-	#The only way I can kill it is forcing a core dump using Ctrl + \
 	def shutdown(self):
+		server.server_close()
 		#Force shutdown, this is the only method which works
 		os._exit(os.EX_OK)
 
@@ -78,7 +76,7 @@ class Server(ThreadPoolMixIn, SocketServer.TCPServer):
 
 if __name__ == "__main__":
 
-	HOST = "localhost"
+	HOST = "0.0.0.0"
 	PORT = int(sys.argv[1])
 	server = Server((HOST, PORT), ThreadedRequestHandler)
 	print "Server started - ", HOST, PORT 
